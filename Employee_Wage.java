ec36a7ba4ab7a7f576b577936259ac7d214b7cc8
Anjali <anjaliap1227@gmail.com>
2024-05-01 23:06:22 +0530

    UC1 added

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,5 +1,12 @@
 public class Employee_Wage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage.");
+        int FULL_TIME_EMP = 1;
+
+        double emp_check = Math.floor(Math.random()*10 % 2);
+        if(FULL_TIME_EMP == emp_check)
+            System.out.println("Employee are present");
+        else
+            System.out.println("Employee are not present");
     }
 }
